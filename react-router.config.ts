38b4792a61aec15d309ba623f6489aa9cc1a7577
Chris Guevara <chrismguevara@gmail.com>
2025-04-17 00:41:59 -0700

    Set basename to /100DaysCSS/

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
+  basename: "/100DaysCSS/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
