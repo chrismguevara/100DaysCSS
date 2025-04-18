f784b6bd0bff6d5e1e9517d547fba0077d7e738b
Chris Guevara <chrismguevara@gmail.com>
2025-04-18 12:08:39 -0700

    Fix link basename issue on landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -57,10 +58,10 @@ export default function Home() {
                   </div>
                   <div className="group relative">
                     <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                      <a href={post.href}>
+                      <Link to={post.href}>
                         <span className="absolute inset-0" />
                         {post.title}
-                      </a>
+                      </Link>
                     </h3>
                     <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">
                       {post.description}
